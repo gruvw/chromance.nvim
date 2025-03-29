7b8bc73560308bf423d3229bd44ac49d718f4306
gruvw <gruvw.dev@gmail.com>
2025-03-29 23:02:54 +0100

    feat: initial commit, modified code, adapt to chromance
    Forked from https://github.com/loctvl842/monokai-pro.nvim/commit/872f774303f79416000e8049630052f4124d9534

---
+++ README.md
@@ -0,0 +1,9 @@
+# Chromance
+
+An opinionated sleek and vibrant Neovim color theme.
+
+**Note** - Inspired by [loctvl842/monokai-pro.nvim](https://github.com/loctvl842/monokai-pro.nvim) Spectrum variant (project forked at [872f774](https://github.com/loctvl842/monokai-pro.nvim/commit/872f774303f79416000e8049630052f4124d9534)).
+
+## Installation
+
+<!-- TODO document install -->
---
+++ lua/chromance/init.lua
@@ -0,0 +1,19 @@
+local config = require("chromance.config")
+local util = require("chromance.util")
+local theme = require("chromance.theme")
+local command = require("chromance.command")
+
+---@class Chromance
+local M = {}
+
+M.load = function()
+  config.extend({ filter = "spectrum" })
+  util.theme.load(theme.setup())
+end
+
+M.setup = function(options)
+  config.setup(options)
+  command.create_filter_command()
+end
+
+return M
